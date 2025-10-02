f35a2df5f0d3265b8fbd46088541a0e7cf7e4b05
yerimlee <144992260+limye2@users.noreply.github.com>
2025-10-02 16:12:27 +0900

    실습6

--- main.c
+++ main.c
@@ -8,29 +8,33 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int a;
-    int b;
-    char op; //int로 받아도 가능
-    int result;
+    int answer = 59;
+    int number;
+    int trial = 0;
     
-    printf("enter the calculation :");
-    scanf("%d %c %d", &a, &op, &b);
+    do {
+        printf("Guess a number :");
+        scanf("%d", &number);
+        
+        trial=trial+1;
+        
+        if(number<answer)
+        {
+            printf("low!\n");
+        }
+        else if(number>answer)
+        {
+            printf("high!\n");
+        }
+        //else
+        //{
+            //printf("Congratulation! trials:%d\n", trial);
+        //}
+        
+        
+    } while (number!=answer); // 위 루프는 정답이 아닐때만 반복
     
-    switch(op)
-    {
-        case '+':
-            result = a+b;
-            break;
-            
-        case '-':
-            result = a-b;
-            break;
-            
-        default:
-            printf("지원하지 않는 연산자입니다.\n");
-    }
-    
-    printf("=%d\n", result);
-    
+    printf("Congratulation! trials:%d\n", trial);
+        
     return 0;
 }
